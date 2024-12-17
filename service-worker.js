3eea849576966176a290bbba3ac90a2339399635
hengle92 <79264847+hengle92@users.noreply.github.com>
2024-12-17 16:00:38 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ const urlsToCache = [
     './styles.css',
     './app.js',
     './manifest.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png'
+    './icon-192.png',
+    './icon-512.png'
 ];
 
 // 安装事件
